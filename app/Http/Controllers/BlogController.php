5a8dc9ab17da36c4c7af95125c61dc93576be7c7
Shakeebraza <Muhammad.Shakeeb@fissionfox.com>
2025-01-10 23:52:15 +0500

    fix:"blog inner page can be done"

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Blog;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class BlogController extends Controller
@@ -20,5 +21,15 @@ class BlogController extends Controller
 
     
         return view('theme.blog.index', compact('blogs'));
+    }
+
+    public function show($slug)
+    {
+     
+        $blog = Blog::with(['author', 'image'])->where('slug', $slug)->firstOrFail();
+
+        $posts = Blog::latest()->take(5)->get(); 
+    
+        return view('theme.blog.show', compact('blog', 'posts'));
     }
 }
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+    protected $table = 'blogs'; 
+
+
+    protected $fillable = [
+        'title',
+        'description',
+        'short_description',
+        'user_id',
+        'location',
+        'image_id',
+    ];
+
+
+    public $timestamps = true;  
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function image()
+    {
+        return $this->belongsTo(Filemanager::class, 'image_id');
+    }
+
+
 }
+
